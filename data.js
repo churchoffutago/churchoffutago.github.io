e13b129982d750ad8720c5f9362b3ae408ef4120
churchoffutago <churchoffutago>
2022-07-28 00:40:45 -0700

    first commit

---
+++ data.js
@@ -0,0 +1,17 @@
+content = [
+    {
+        'title': 'sadfutago cant kill block guy',
+        'time': '07-27-2022 8PM PST',
+        'content': '<iframe id="reddit-embed" src="https://www.redditmedia.com/r/nier/comments/w9yosf/i_played_as_9s_after_doing_missions_to_come_back/?ref_source=embed&amp;ref=share&amp;embed=true&amp;showmedia=false" sandbox="allow-scripts allow-same-origin allow-popups" style="border: none;" height="182" width="640" scrolling="no"></iframe>'
+    },
+    {
+        'title': 'sadfutago beats bloby',
+        'time': '07-27-2022 11PM PST',
+		'content': '<iframe id="reddit-embed" src="https://www.redditmedia.com/r/nier/comments/wa11po/i_think_my_game_has_a_glitch_since_i_keep_going/?ref_source=embed&amp;ref=share&amp;embed=true&amp;showmedia=false" sandbox="allow-scripts allow-same-origin allow-popups" style="border: none;" height="182" width="640" scrolling="no"></iframe>'
+    },
+    {
+        'title': '"I like it"',
+        'time': '07-27-2022 11PM PST',
+		'content': '<iframe id="reddit-embed" src="https://www.redditmedia.com/r/nier/comments/w9l2aw/not_church_related_just_my_2b_fanart/ihyfvyu/?depth=1&amp;showmore=false&amp;embed=true&amp;showtitle=true&amp;showmedia=false" sandbox="allow-scripts allow-same-origin allow-popups" style="border: none;" height="237" width="640" scrolling="no"></iframe>'
+    }
+];
